aeb3dc02226472b10c59d453a02fbef6cebd2dbf
SanaboinaSwetha <126000859+SanaboinaSwetha@users.noreply.github.com>
2023-07-19 21:36:25 +0530

    Add files via upload

---
+++ i.js
@@ -0,0 +1,9 @@
+function phone(){
+    document.getElementById("bulb").src="https://asia-exstatic-vivofs.vivo.com/PSee2l50xoirPK7y/1677055992779/bd5979b2c8afc4131fa265fcbbe12de8.png";
+}
+function tab(){
+    document.getElementById("bulb").src="https://m.media-amazon.com/images/I/61-y844vGPL._SX679_.jpg";
+}
+function laptop(){
+    document.getElementById("bulb").src="https://4.imimg.com/data4/SJ/BA/MY-3018414/apple-laptop.jpg";
+}
